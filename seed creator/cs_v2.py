d383d4e43f646eee4d46e759931bd6048d8b7656
Andrewson <andrewson@ackcio.com>
2023-07-12 04:49:34 +0530

    fix

--- seed creator/cs_v2.py
+++ seed creator/cs_v2.py
@@ -5,17 +5,19 @@ def generate_input():
   special_values = [float('NaN'), float('Infinity'), float('-Infinity')]
   boundary_values = [1.1754943508222875e-38, -1.1754943508222875e-38]
   random_values = [random.random() for _ in range(10)]
+  rv = random.choice(random_values)
+  re = random.choice([float('NaN'), float('Infinity'), float('-Infinity'),1.1754943508222875e-38, -1.1754943508222875e-38])
 
   #return random.choice([special_values, boundary_values, random_values])
-  return random.choice([float('NaN'), float('Infinity'), float('-Infinity'),1.1754943508222875e-38, -1.1754943508222875e-38, random_values])
+  return random.choice([re, rv])
 
 def main():
   """Generates a corpus of 1000 floating point inputs."""
   #corpus = []
   corpus_str=""
-  for _ in range(1000):
+  for _ in range(100):
     #corpus.append(generate_input())
-    corpus_str = corpus_str + "," + str(generate_input())
+    corpus_str = corpus_str + "\n" + str(generate_input())
 
   corpus_str = corpus_str [1:]
   with open('input.corpus', 'w') as f:
